bd54abb2e76db426d962272ff340dbf116d77dad
Carlo Contardi <c.contardi@spacecoding.it>
2024-12-26 15:50:27 +0100

    [Feature] Add FileHelper

--- app/src/main/java/it/spacecoding/todolist/MainActivity.java
+++ app/src/main/java/it/spacecoding/todolist/MainActivity.java
@@ -1,15 +1,26 @@
 package it.spacecoding.todolist;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    EditText mItem;
+    Button mAdd;
+    ListView mListView;
+    ArrayList<String> mItemLIst = new ArrayList<>();
+    ArrayAdapter<String> mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        mItem = findViewById(R.id.editTextText);
+        mAdd = findViewById(R.id.button);
+        mListView = findViewById(R.id.list);
+
     }
 }
